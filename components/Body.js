fea7d2fc1def1e722164cac7013ece15612eb7d1
Chad Woolley <cwoolley@gitlab.com>
2022-12-15 02:02:54 -0800

    Avoid react warning about useEffect being async

--- components/Body.js
+++ components/Body.js
@@ -1,14 +1,18 @@
 import {sayHello} from "../lib/api_client";
 import Header from "@components/Header";
-import { useState, useEffect } from 'react'
+import {useState, useEffect} from 'react'
 
 export default function Footer() {
   const [message, setMessage] = useState('Loading...');
 
-  useEffect(async () => {
-    setMessage(await sayHello())
+  useEffect(() => {
+    async function effect() {
+      setMessage(await sayHello())
+    }
+    // See note on 'void' here: https://stackoverflow.com/a/64234381/25192
+    void effect();
   }, [])
-  
+
   return (
     <main>
       <Header title="This is the message returned from the serverless function:"/>
